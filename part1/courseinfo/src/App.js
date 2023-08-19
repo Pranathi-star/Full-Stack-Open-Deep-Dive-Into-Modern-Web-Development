61bedf88de8222d83f24e0ace185d335bd8c0710
Pranathi-star <pranathik2001@gmail.com>
2023-08-19 15:38:17 +0530

    Solution for exercise 1.3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -29,21 +29,27 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
-    <div>
-      <Header course = {course} />
-      <Content partsList = {[part1, part2, part3]} exercisesList = {[exercises1, exercises2, exercises3]} />
-      <Total total = {exercises1 + exercises2 + exercises3} />
-    </div>
-  )
-}
+    const course = 'Half Stack application development'
+    const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
+    }
+    const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    const part3 = {
+      name: 'State of a component',
+      exercises: 14
+    }
+  
+    return (
+      <>
+        <Header course = {course} />
+        <Content partsList = {[part1.name, part2.name, part3.name]} exercisesList = {[part1.exercises, part2.exercises, part3.exercises]} />
+        <Total total = {part1.exercises + part2.exercises + part3.exercises} />
+      </>
+    )
+  }
 
 export default App
