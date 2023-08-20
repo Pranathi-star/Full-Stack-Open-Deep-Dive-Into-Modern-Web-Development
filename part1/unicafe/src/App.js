12e35fff7487605eb19c0f6e62509dc74a671177
Pranathi-star <pranathik2001@gmail.com>
2023-08-20 09:12:48 +0530

    Solution for Exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,16 +12,22 @@ const Button = ({handleClick, text}) => {
   )
 }
 
-const Statistics = ({good, neutral, bad, all, text}) => {
+const StatisticLine = ({text, value}) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
+
+const Statistics = ({good, neutral, bad, all}) => {
   if (all !== 0){
     return (
       <>
-        <p>{text[0]} {good}</p>
-        <p>{text[1]} {neutral}</p>
-        <p>{text[2]} {bad}</p>
-        <p>{text[3]} {all}</p>
-        <p>{text[4]} {(good - bad) / all}</p>
-        <p>{text[5]} {(good / all) * 100} %</p>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={all}/>
+        <StatisticLine text="average" value={(good - bad) / all}/>
+        <StatisticLine text="positive" value={(good * 100) / all}/>
       </>
     )
   }
@@ -38,19 +44,18 @@ const App = () => {
 
   const feedbackHeading = "give feedback";
   const statsHeading = "statistics";
-  const text = ["good", "neutral", "bad", "all", "average", "positive"];
   const func = [() => setGood(good + 1), () => setNeutral(neutral + 1), () => setBad(bad + 1)]
 
   return (
     <div>
       <Heading text={feedbackHeading} />
       <div styles="display: inline-block">
-        <Button handleClick = {func[0]} text = {text[0]}/>
-        <Button handleClick = {func[1]} text = {text[1]}/>
-        <Button handleClick = {func[2]} text = {text[2]}/>
+        <Button handleClick = {func[0]} text = "good"/>
+        <Button handleClick = {func[1]} text = "neutral"/>
+        <Button handleClick = {func[2]} text = "bad"/>
       </div>
       <Heading text={statsHeading} />
-      <Statistics good = {good} neutral= {neutral} bad = {bad} all = {good + bad + neutral} text = {text}/>
+      <Statistics good = {good} neutral= {neutral} bad = {bad} all = {good + bad + neutral} />
     </div>
   )
 }
