9e13f0d7ece9190a70da47f5082abf932f93e7bd
Pranathi-star <pranathik2001@gmail.com>
2023-08-21 08:48:19 +0530

    Solution for Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ handleFilter }) => {
   return (
@@ -35,18 +36,23 @@ const Persons = ({ filtered }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
 
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filtered, setFiltered] = useState(persons)
 
   let alreadyPresent = false;
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log(response.data)
+        setPersons(response.data)
+        setFiltered(response.data)
+      })
+  }, [])
 
   const handleSubmit = (event) => {
     event.preventDefault();
