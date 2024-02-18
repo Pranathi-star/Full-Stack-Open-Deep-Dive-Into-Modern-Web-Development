6191c25cf55366a00ecef8851d871c7231e39389
Pranathi-star <pranathik2001@gmail.com>
2024-02-18 19:21:12 +0530

    Solution for Exercises 6.14-6.15

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,16 +1,18 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { createNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault();
         var content = event.target.anecdote.value
         console.log(content)
         event.target.anecdote.value = ''
-        dispatch(createAnecdote(content))
+        const newNote = await anecdoteService.createAnecdote(content)
+        dispatch(createAnecdote(newNote))
         var message = `anecdote "${content}" successfully added!`
         dispatch(createNotification(message))
     }
